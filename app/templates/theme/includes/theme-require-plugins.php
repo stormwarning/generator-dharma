d174ba77c0b10dd8acd7a63c94d3443332090c61
Jeff <rustyangel@gmail.com>
2014-11-28 11:27:04 -0700

    add smushit and wpremote to required plugins
    also, tabs → spaces

--- app/templates/theme/includes/theme-require-plugins.php
+++ app/templates/theme/includes/theme-require-plugins.php
@@ -4,112 +4,126 @@
  */
 function om_register_required_plugins() {
 
-	$plugins = array(
+  $plugins = array(
 
-		array(
-			'name'             => 'Advanced Custom Fields',
-			'slug'             => 'advanced-custom-fields',
-			'required'         => true,
-			'force_activation' => true
-		),
+    array(
+      'name'             => 'Advanced Custom Fields',
+      'slug'             => 'advanced-custom-fields',
+      'required'         => true,
+      'force_activation' => true
+    ),
 
-		array(
-			'name'             => 'WordPress SEO by Yoast',
-			'slug'             => 'wordpress-seo',
-			'required'         => true,
-			'force_activation' => true
-		),
+    array(
+      'name'             => 'WordPress SEO by Yoast',
+      'slug'             => 'wordpress-seo',
+      'required'         => true,
+      'force_activation' => true
+    ),
 
-	);
+    array(
+      'name'             => 'WP Smush.it',
+      'slug'             => 'wp-smushit',
+      'required'         => true,
+      'force_activation' => true
+    ),
 
-	// Change this to your theme text domain, used for internationalising strings
-	$theme_text_domain = 'om';
+    array(
+      'name'             => 'WP Remote',
+      'slug'             => 'wpremote',
+      'required'         => true,
+      'force_activation' => true
+    )
 
-	/**
-	 * Array of configuration settings. Amend each line as needed.
-	 *
-	 * If you want the default strings to be available under your own theme
-	 * domain, leave the strings uncommented.
-	 *
-	 * Some of the strings are added into a sprintf, so see the comments at the
-	 * end of each line for what each argument will be.
-	 */
-	$config = array(
+  );
 
-		// Text domain - likely want to be the same as your theme.
-		'domain'           => $theme_text_domain,
+  // Change this to your theme text domain, used for internationalising strings
+  $theme_text_domain = 'om';
 
-		// Default absolute path to pre-packaged plugins
-		'default_path'     => '',
+  /**
+   * Array of configuration settings. Amend each line as needed.
+   *
+   * If you want the default strings to be available under your own theme
+   * domain, leave the strings uncommented.
+   *
+   * Some of the strings are added into a sprintf, so see the comments at the
+   * end of each line for what each argument will be.
+   */
+  $config = array(
 
-		// Default parent menu slug
-		'parent_menu_slug' => 'themes.php',
+    // Text domain - likely want to be the same as your theme.
+    'domain'           => $theme_text_domain,
 
-		// Default parent URL slug
-		'parent_url_slug'  => 'themes.php',
+    // Default absolute path to pre-packaged plugins
+    'default_path'     => '',
 
-		// Menu slug
-		'menu'             => 'install-required-plugins',
+    // Default parent menu slug
+    'parent_menu_slug' => 'themes.php',
 
-		// Show admin notices or not
-		'has_notices'      => true,
+    // Default parent URL slug
+    'parent_url_slug'  => 'themes.php',
 
-		// Automatically activate plugins after installation or not
-		'is_automatic'     => false,
+    // Menu slug
+    'menu'             => 'install-required-plugins',
 
-		// Message to output right before the plugins table
-		'message'          => '',
+    // Show admin notices or not
+    'has_notices'      => true,
 
-		'strings' => array(
-			'page_title' => __( 'Install Required Plugins', $theme_text_domain ),
+    // Automatically activate plugins after installation or not
+    'is_automatic'     => false,
 
-			'menu_title' => __( 'Install Plugins', $theme_text_domain ),
+    // Message to output right before the plugins table
+    'message'          => '',
 
-			// %1$s = plugin name
-			'installing' => __( 'Installing Plugin: %s', $theme_text_domain ),
+    'strings' => array(
+      'page_title' => __( 'Install Required Plugins', $theme_text_domain ),
 
-			'oops' => __( 'Something went wrong with the plugin API.', $theme_text_domain ),
+      'menu_title' => __( 'Install Plugins', $theme_text_domain ),
 
-			// %1$s = plugin name(s)
-			'notice_can_install_required' => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ),
+      // %1$s = plugin name
+      'installing' => __( 'Installing Plugin: %s', $theme_text_domain ),
 
-			// %1$s = plugin name(s)
-			'notice_can_install_recommended' => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ),
+      'oops' => __( 'Something went wrong with the plugin API.', $theme_text_domain ),
 
-			// %1$s = plugin name(s)
-			'notice_cannot_install' => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ),
+      // %1$s = plugin name(s)
+      'notice_can_install_required' => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ),
 
-			// %1$s = plugin name(s)
-			'notice_can_activate_required' => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ),
+      // %1$s = plugin name(s)
+      'notice_can_install_recommended' => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ),
 
-			// %1$s = plugin name(s)
-			'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ),
+      // %1$s = plugin name(s)
+      'notice_cannot_install' => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ),
 
-			// %1$s = plugin name(s)
-			'notice_cannot_activate' => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ),
+      // %1$s = plugin name(s)
+      'notice_can_activate_required' => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ),
 
-			// %1$s = plugin name(s)
-			'notice_ask_to_update' => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ),
+      // %1$s = plugin name(s)
+      'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ),
 
-			// %1$s = plugin name(s)
-			'notice_cannot_update' => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ),
+      // %1$s = plugin name(s)
+      'notice_cannot_activate' => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ),
 
-			'install_link' => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+      // %1$s = plugin name(s)
+      'notice_ask_to_update' => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ),
 
-			'activate_link' => _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
+      // %1$s = plugin name(s)
+      'notice_cannot_update' => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ),
 
-			'return' => __( 'Return to Required Plugins Installer', $theme_text_domain ),
-			'plugin_activated' => __( 'Plugin activated successfully.', $theme_text_domain ),
+      'install_link' => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
 
-			// %1$s = dashboard link
-			'complete' => __( 'All plugins installed and activated successfully. %s', $theme_text_domain ),
+      'activate_link' => _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
 
-			// Determines admin notice type - can only be 'updated' or 'error'
-			'nag_type' => 'updated'
-		)
+      'return' => __( 'Return to Required Plugins Installer', $theme_text_domain ),
+      'plugin_activated' => __( 'Plugin activated successfully.', $theme_text_domain ),
 
-	);
+      // %1$s = dashboard link
+      'complete' => __( 'All plugins installed and activated successfully. %s', $theme_text_domain ),
 
-	tgmpa( $plugins, $config );
+      // Determines admin notice type - can only be 'updated' or 'error'
+      'nag_type' => 'updated'
+    )
+
+  );
+
+  tgmpa( $plugins, $config );
 
 }
