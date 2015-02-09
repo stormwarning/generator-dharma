5c757a5c4827b4a9c72527cf0fd04ca60fde5d17
Jeff <rustyangel@gmail.com>
2015-02-09 12:04:23 -0700

    add revision config & update formatting; fixes #13

--- app/templates/_wp-config.php
+++ app/templates/_wp-config.php
@@ -1,34 +1,41 @@
 <?php
-// ===================================================
-// Load database info and local development parameters
-// ===================================================
+// DATABASE INFO AND LOCAL DEV PARAMETERS =============================
 if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
-	define( 'WP_LOCAL_DEV', true );
-	include( dirname( __FILE__ ) . '/local-config.php' );
+
+  define( 'WP_LOCAL_DEV', true );
+  include( dirname( __FILE__ ) . '/local-config.php' );
+
 } else {
-	define( 'WP_LOCAL_DEV', false );
-	define( 'DB_NAME', '<%= dbName %>' );
-	define( 'DB_USER', '<%= dbUser %>' );
-	define( 'DB_PASSWORD', '<%= dbPass %>' );
-	define( 'DB_HOST', <%= dbHost %> ); // Probably 'localhost'
+
+  define( 'WP_LOCAL_DEV', false );
+  define( 'DB_NAME', '<%= dbName %>' );
+  define( 'DB_USER', '<%= dbUser %>' );
+  define( 'DB_PASSWORD', '<%= dbPass %>' );
+  define( 'DB_HOST', <%= dbHost %> ); // Probably 'localhost'
+
 }
 
-// ========================
-// Custom Content Directory
-// ========================
+
+// CUSTOM CONTENT DIRECTORY ===========================================
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
-// ================================================
-// You almost certainly do not want to change these
-// ================================================
+
+// CUSTOM CONFIGURATIONS ==============================================
+// Prevent file editing via the dashboard editor.
+define( 'DISALLOW_FILE_EDIT', true );
+
+// Adjust post revisioning and autosaving.
+define( 'AUTOSAVE_INTERVAL', 300 );
+define( 'WP_POST_REVISIONS', 3 );
+
+
+// PROBABLY NEVER CHANGE THESE ========================================
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-// ==============================================================
-// Salts, for security
+// SALTS, FOR SECURITY ================================================
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
-// ==============================================================
 define('AUTH_KEY',         'put your unique phrase here');
 define('SECURE_AUTH_KEY',  'put your unique phrase here');
 define('LOGGED_IN_KEY',    'put your unique phrase here');
@@ -37,46 +44,36 @@ define('AUTH_SALT',        'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
 define('NONCE_SALT',       'put your unique phrase here');
-// ==============================================================
-// Table prefix
+
+
+// DATABASE TABLE PREFIX ==============================================
 // Change this if you have multiple installs in the same database
-// ==============================================================
 $table_prefix  = '<%= dbPrefix %>';
 
-// ================================
-// Language
+
+// LANGUAGE ===========================================================
 // Leave blank for American English
-// ================================
 define( 'WPLANG', '' );
 
-// ===========
-// Hide errors
-// ===========
+
+// HIDE ERRORS ========================================================
 ini_set( 'display_errors', 0 );
 define( 'WP_DEBUG_DISPLAY', false );
 
-// =================================================================
-// Debug mode
+
+// DEBUG MODE =========================================================
 // Debugging? Enable these. Can also enable them in local-config.php
-// =================================================================
 // define( 'SAVEQUERIES', true );
 // define( 'WP_DEBUG', true );
 
-// ======================================
-// Load a Memcached config if we have one
-// ======================================
+
+// LOAD A MEMCACHED CONFIG IF WE HAVE ONE =============================
 if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
-	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
+  $memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
 
-// ===========================================================================================
-// This can be used to programatically set the stage when deploying (e.g. production, staging)
-// ===========================================================================================
-define( 'WP_STAGE', '%%WP_STAGE%%' );
-define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
 
-// ===================
-// Bootstrap WordPress
-// ===================
+// BOOTSTRAP WORDPRESS ================================================
 if ( !defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname( __FILE__ ) . '/<%= _.slugify(wpDirectory) %>/' );
+  define( 'ABSPATH', dirname( __FILE__ ) . '/<%= _.slugify(wpDirectory) %>/' );
+
 require_once( ABSPATH . 'wp-settings.php' );
