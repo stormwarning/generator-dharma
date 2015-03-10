eef967ce4f5e6eb36e5c0100ed2e6060349dbc8a
Jeff <rustyangel@gmail.com>
2015-03-10 14:10:42 -0600

    add custom login page styles & functions; fixes #7

--- app/templates/theme/includes/om-dashboard.php
+++ app/templates/theme/includes/om-dashboard.php
@@ -3,22 +3,67 @@
  * WordPress Dashboard modifications
  * Customising login screens, and other wp-admin hooks
  *
- * @package Overhaul
- * @since 3.9.1
+ * @package dharma
  */
+
+
+/**
+ * Customise the login screen.
+ */
+add_action( 'login_head', 'om_login_styles' );
+add_filter( 'login_headerurl', 'om_login_logourl' );
+add_filter( 'login_headertitle', 'om_login_logotitle' );
+
+if ( ! function_exists( 'om_login_styles' ) ) {
+
+  function om_login_logo() {
+
+    echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/assets/styles/login-style.css">';
+
+  }
+
+}
+
+
+if ( ! function_exists( 'om_login_logourl' ) ) {
+
+  function om_login_logourl() {
+
+    return 'http://overhaulmedia.com/';
+
+  }
+
+}
+
+
+if ( ! function_exists( 'om_login_logotitle' ) ) {
+
+  function om_login_logotitle() {
+
+    return 'From your friends at Overhaul Media';
+
+  }
+
+}
+
+
+
 
 /**
  * Add Overhaul contact notice to Core Update notice
  * First, remove the default notice.
  * Next, replicate the original function, with our updated notice text.
  *
+ * @since 3.9.1
  * @todo Find a way to append to the original notice instead of duplicating
  */
 add_action( 'admin_init', 'remove_default_update_nag' );
 
 function remove_default_update_nag() {
-	remove_action( 'admin_notices', 'update_nag', 3 );
-	remove_action( 'network_admin_notices', 'update_nag', 3 );
+
+  remove_action( 'admin_notices', 'update_nag', 3 );
+  remove_action( 'network_admin_notices', 'update_nag', 3 );
+
 }
 
 
@@ -26,24 +71,30 @@ add_action( 'admin_notices', 'om_update_nag', 3 );
 add_action( 'network_admin_notices', 'om_update_nag', 3 );
 
 function om_update_nag() {
-	if ( is_multisite() && !current_user_can('update_core') )
-		return false;
 
-	global $pagenow;
+  if ( is_multisite() && ! current_user_can('update_core') )
+    return false;
 
-	if ( 'update-core.php' == $pagenow )
-		return;
+  global $pagenow;
 
-	$cur = get_preferred_from_update_core();
+  if ( 'update-core.php' == $pagenow )
+    return;
 
-	if ( ! isset( $cur->response ) || $cur->response != 'upgrade' )
-		return false;
+  $cur = get_preferred_from_update_core();
 
-	if ( current_user_can('update_core') ) {
-		$msg = sprintf( __('<a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> is available! <a href="%2$s">Please update now</a>.<br>Reach out to your friends at <a href="http://overhaulmedia.com/" target="_blank">Overhaul Media</a> for assistance!'), $cur->current, network_admin_url( 'update-core.php' ) );
-	} else {
-		$msg = sprintf( __('<a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> is available! Please notify the site administrator.<br>Reach out to your friends at <a href="http://overhaulmedia.com/" target="_blank">Overhaul Media</a> for assistance!'), $cur->current );
-	}
-	echo '<div class="update-nag">' . $msg . '</div>';
+  if ( ! isset( $cur->response ) || $cur->response != 'upgrade' )
+    return false;
+
+  if ( current_user_can('update_core') ) {
+
+    $msg = sprintf( __('<a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> is available! <a href="%2$s">Please update now</a>.<br>Reach out to your friends at <a href="http://overhaulmedia.com/" target="_blank">Overhaul Media</a> for assistance!'), $cur->current, network_admin_url( 'update-core.php' ) );
+
+  } else {
+
+    $msg = sprintf( __('<a href="http://codex.wordpress.org/Version_%1$s">WordPress %1$s</a> is available! Please notify the site administrator.<br>Reach out to your friends at <a href="http://overhaulmedia.com/" target="_blank">Overhaul Media</a> for assistance!'), $cur->current );
+
+  }
+
+  echo '<div class="update-nag">' . $msg . '</div>';
+
 }
-?>
