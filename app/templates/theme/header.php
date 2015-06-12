f99191012f6d943a4ce64e95d17e09682831ca53
Jeff <rustyangel@gmail.com>
2015-06-12 12:14:24 -0600

    Remove extraneous `<meta>` tags

--- app/templates/theme/header.php
+++ app/templates/theme/header.php
@@ -8,24 +8,13 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
 
-    <!-- Add to homescreen for Chrome on Android -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <link rel="icon" sizes="196x196" href="/chrome-touch-icon-196x196.png">
-
-    <!-- Add to homescreen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <meta name="apple-mobile-web-app-title" content="">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-precomposed.png">
-
-    <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <meta name="msapplication-TileImage" content="/ms-tile-144x144.png">
-    <meta name="msapplication-TileColor" content="#2661aa">
-
     <meta name="application-name" content="">
 
 
@@ -36,10 +25,10 @@
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
 
-    <a class="skip-link screen-reader-text" href="#main">Skip to content</a>
+    <a class="skip-navigation" href="#main">Skip to content</a>
 
 
-    <header class="site-header" id="top">
+    <header class="site-header" id="top" role="banner">
       <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
         <?php bloginfo( 'name' ); ?>
       </a>
