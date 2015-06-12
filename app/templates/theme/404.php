47133a811048c34e06c25edb4d81bb3b58d5e23f
Jeff <rustyangel@gmail.com>
2015-06-12 12:11:52 -0600

    Include basic search form

--- app/templates/theme/404.php
+++ app/templates/theme/404.php
@@ -5,11 +5,16 @@
 ?>
 
 
-<main class="main main-404" id="main" role="main">
+<main class="main  main-404" id="main" role="main">
 
   <h1 class="page-title">Oops! That page can&rsquo;t be found.</h1>
 
-  <p>It looks like nothing was found at this location. <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Return home</a> and try again.
+  <p>It looks like nothing was found at this location. <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Return home</a> or try a search.</p>
+
+  <form class="site-search" role="search" method="GET" action="/">
+    <input class="search-field" type="search" name="s" value placeholder="Search for:" title="Search for:">
+    <button class="search-submit" type="submit">Go</button>
+  </form>
 
 </main>
 
